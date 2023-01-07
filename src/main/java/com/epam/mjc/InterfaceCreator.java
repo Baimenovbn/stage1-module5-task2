40992f63f5122443f562263a4d66b5edff787bac
Baimenovbn <baibn@mail.ru>
2023-01-07 20:59:10 +0600

    feat: finish task

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,16 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (array) -> {
+            ArrayList<Integer> dividedArray = new ArrayList<>();
+
+            array.forEach(el -> dividedArray.add(el / divider));
+
+            return dividedArray;
+        };
     }
 }
